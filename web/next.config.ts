e87a3045df7bdaf41c3a6b09e193fe14a3e6aae2
Przemysław Kamiński <92935278+Shembou@users.noreply.github.com>
2024-10-31 09:47:03 +0100

    fix: next-mdx-remote package fix

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     domains: ['cdn.sanity.io']
-  }
+  },
+  transpilePackages: ['next-mdx-remote']
 }
 
 export default nextConfig
