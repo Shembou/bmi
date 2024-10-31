2b1e26a303edd13544fc776712a94fb1e32ae90d
Przemysław Kamiński <92935278+Shembou@users.noreply.github.com>
2024-10-31 10:57:34 +0100

    fix: react key items

--- web/src/components/TilesSection/Tiles.tsx
+++ web/src/components/TilesSection/Tiles.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react'
 import Markdown from '../common/Markdown/Markdown'
 import { ITile } from './ITilesSection'
 
@@ -8,12 +9,11 @@ export default function Tiles({ tiles }: { tiles: ITile[] }) {
   const arrowStrokeColors = ['#C1DADC', '#AFBEE3', '#C3DABA']
 
   return (
-    <div className="grid gap-6 justify-center grid-cols-23 h-full py-24">
+    <div className="grid gap-6 justify-center grid-cols-23 h-full py-24" key={0}>
       {tiles.map(({ heading, description }, index) =>
         index % 2 === 0 ? (
-          <>
+          <Fragment key={index}>
             <div
-              key={index}
               className={`bg-${colors[index % 3]} border-${borderColors[index % 3]} grid gap-2 ${index == 4 && 'col-span-11'} ${index == 0 && 'col-span-7'} ${index == 2 && 'col-span-10'} ${index + 1 == tiles.length && 'col-span-full max-w-xl justify-self-center'} p-8 rounded-xl border`}
             >
               <div className="flex gap-2">
@@ -28,7 +28,7 @@ export default function Tiles({ tiles }: { tiles: ITile[] }) {
                 strokeColor={arrowStrokeColors[index % 3]}
               />
             )}
-          </>
+          </Fragment>
         ) : (
           <div
             key={index}
