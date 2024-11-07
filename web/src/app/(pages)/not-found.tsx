87a14884911ae2376fa860ee442304d3e50f9756
Przemysław Kamiński <92935278+Shembou@users.noreply.github.com>
2024-11-07 13:36:56 +0100

    fix: notFoundPage

--- web/src/app/(pages)/not-found.tsx
+++ web/src/app/(pages)/not-found.tsx
@@ -8,7 +8,7 @@ export default async function NotFound() {
   const data = await getCmsData<INotFound>({ query: GetNotFoundPageData })
 
   return (
-    <main>
+    <main className="w-full mb-24">
       <NotFoundComponent {...data} />
       <Components content={data.content} />
     </main>
