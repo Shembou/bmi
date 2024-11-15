511b3fe270b94ac681ebb48e35903151c0194f36
Przemysław Kamiński <92935278+Shembou@users.noreply.github.com>
2024-11-15 12:58:41 +0100

    func: added pagination to videosSection

--- web/src/components/VideoSection/VideoSection.tsx
+++ web/src/components/VideoSection/VideoSection.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { useState } from 'react'
 import { IVideoSection } from './IVideoSection'
+import Pagination from '../common/Pagination/Pagination'
 
 export default function VideoSection({ videos }: IVideoSection) {
   const formatYouTubeURL = (urlOrId: string) => {
@@ -13,22 +17,39 @@ export default function VideoSection({ videos }: IVideoSection) {
     return `https://www.youtube.com/embed/${urlOrId}`
   }
 
+  const itemsPerPage = 6
+  const [itemsToShow, setItemsToShow] = useState(itemsPerPage)
+  const handleLoadMore = () => {
+    setItemsToShow(itemsToShow + itemsPerPage)
+  }
+
   return (
-    <section className={'grid gap-9 grid-cols-1 xl:grid-cols-3 py-24 sm:grid-cols-2'}>
-      {videos &&
-        videos.map(({ description, title, url }, index) => (
-          <div key={index}>
-            <iframe
-              height={310}
-              src={formatYouTubeURL(url)}
-              className="rounded-3xl w-full"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-            <h4 className="pt-3">{title}</h4>
-            <p className="p3-3">{description}</p>
-          </div>
-        ))}
-    </section>
+    <>
+      <section>
+        <div className={'grid gap-9 grid-cols-1 xl:grid-cols-3 py-24 sm:grid-cols-2'}>
+          {videos &&
+            videos.slice(0, itemsToShow).map(({ description, title, url }, index) => (
+              <div key={index}>
+                <iframe
+                  height={310}
+                  src={formatYouTubeURL(url)}
+                  className="rounded-3xl w-full"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+                <h4 className="pt-3">{title}</h4>
+                <p className="p3-3">{description}</p>
+              </div>
+            ))}
+        </div>
+        {itemsToShow < videos.length && (
+          <Pagination
+            itemsToShow={itemsToShow}
+            length={videos.length}
+            handleClick={handleLoadMore}
+          />
+        )}
+      </section>
+    </>
   )
 }
