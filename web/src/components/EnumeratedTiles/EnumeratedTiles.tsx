1505883eeb27fae76456e1bf8842b09189e6c787
Przemysław Kamiński <92935278+Shembou@users.noreply.github.com>
2024-12-10 17:20:49 +0100

    chore: made heading sticky

--- web/src/components/EnumeratedTiles/EnumeratedTiles.tsx
+++ web/src/components/EnumeratedTiles/EnumeratedTiles.tsx
@@ -4,7 +4,7 @@ import { IEnumeratedTiles } from './IEnumeratedTiles'
 export default function EnumeratedTiles({ heading, tiles }: IEnumeratedTiles) {
   return (
     <section className="flex justify-between py-24 lg:flex-row flex-col gap-5" id="OProjekcie">
-      <h2>{heading}</h2>
+      <h2 className="relative lg:top-7 h-min lg:sticky min-w-64">{heading}</h2>
       <div className="gap-16 justify-items-end grid ">
         {tiles.map(({ heading, description }, i) => (
           <div key={i} className="grid gap-6 md:grid-flow-col grid-flow-row items-start">
